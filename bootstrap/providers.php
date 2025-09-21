c07ffc19b18793e467dc1be51561cb8305a00842
Kakada <nkakada@outlook.com>
2025-09-21 12:03:54 +0700

    Add AppServiceProvider to providers list
    Included App\Providers\AppServiceProvider in the providers array to ensure its services are registered during application bootstrap.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\ApiServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Jenssegers\Agent\AgentServiceProvider::class,
 ];
